d49550990f83054422376fc0cdfb4d75a2fdbb97
shawon046 <shawon56512@gmail.com>
2021-05-04 16:53:57 +0600

    reverse counter working

--- ang.js
+++ ang.js
@@ -1,23 +1,11 @@
-// var app = angular.module('clock', []);
-
-//   app.controller("digitalClock", function($timeout, dateFilter) {
-//     return {
-//       restrict: 'E',
-//       link: function(scope, iElement) {
-//         (function updateClock() {
-//           iElement.text(dateFilter(new Date(), 'H:mm:ss'));
-//           $timeout(updateClock, 1000);
-//         })();
-//       }
-//     };
-//   });
-
 var module = angular.module('clock', []);
 
 module.controller('digitalClock', function($scope, $interval) {
-  var tick = function() {
-    $scope.clock = Date.now();
-  }
-  tick();
-  $interval(tick, 1000);
+    // counts 100 sec
+    $scope.CountDown = 100;
+    $interval(function () {
+        if ($scope.CountDown > 0) {
+            $scope.CountDown--;
+        }
+    }, 1000, 0);
 });
