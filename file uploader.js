f75d9029e8afceb93efa6350946da2b277195ead
shawon046 <shawon56512@gmail.com>
2021-05-06 14:39:37 +0600

    trying to create a server

--- file uploader.js
+++ file uploader.js
@@ -1,13 +1,31 @@
 var myApp = angular.module('uploadApp', []);
 
 
-myApp.controller('uploadCtrl', ['$scope', 'fileUpload', function($scope, fileUpload) {
-$scope.uploadFile = function() {
+// myApp.controller('uploadCtrl', ['$scope', 'fileUpload', function($scope, fileUpload) {
+// $scope.uploadFile = function() {
     
-    var file = $scope.myFile;
-    console.log('file is ' );
-    console.dir(file);
-    var uploadUrl = "/fileUpload";
-    fileUpload.uploadFileToUrl(file, uploadUrl);
-};
-}]);
+//     var file = $scope.myFile;
+//     console.log('file is ' );
+//     console.dir(file);
+//     var uploadUrl = "/fileUpload";
+//     fileUpload.uploadFileToUrl(file, uploadUrl);  
+//     };
+// }]);
+
+
+myApp.directive('validfile', function validFile() {
+    var validFormats = ['txt', 'gif'];
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            ctrl.$validators.validFile = function() {
+                elem.on('change', function () {
+                   var value = elem.val(),
+                       ext = value.substring(value.lastIndexOf('.') + 1).toLowerCase();   
+
+                   return validFormats.indexOf(ext) !== -1;
+                });
+           };
+        }
+    };
+});
