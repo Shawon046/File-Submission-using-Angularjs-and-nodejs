e9fa14f212ffb8d8d144e1b4c6921c56abe5740f
shawon046 <shawon56512@gmail.com>
2021-05-06 16:04:45 +0600

    simple soln.html works fine for fixed file type

--- file uploader.js
+++ file uploader.js
@@ -1,31 +1,26 @@
 var myApp = angular.module('uploadApp', []);
 
+myApp.controller('uploadCtrl', function($scope) {
+    const validFormats = ['txt','doc','png'];
+    $scope.setFile = function(element) {
+        $scope.$apply(function($scope) {
+            $scope.theFile = element.files[0];
+            $scope.FileMessage = '';
+            var filename = $scope.theFile.name;
+            console.log(filename.length)
+            var index = filename.lastIndexOf(".");
+            var file_extension = filename.substring(index, filename.length);
+            if (validFormats.indexOf(file_extension) == -1)
+            {
+                console.log('File Uploaded sucessfully');
+            }
+            else {
+                $scope.theFile = '';
+                    $scope.FileMessage = 'please upload correct File Name, File extension should be in fixed format';
+            }
 
-// myApp.controller('uploadCtrl', ['$scope', 'fileUpload', function($scope, fileUpload) {
-// $scope.uploadFile = function() {
-    
-//     var file = $scope.myFile;
-//     console.log('file is ' );
-//     console.dir(file);
-//     var uploadUrl = "/fileUpload";
-//     fileUpload.uploadFileToUrl(file, uploadUrl);  
-//     };
-// }]);
-
-
-myApp.directive('validfile', function validFile() {
-    var validFormats = ['txt', 'gif'];
-    return {
-        require: 'ngModel',
-        link: function (scope, elem, attrs, ctrl) {
-            ctrl.$validators.validFile = function() {
-                elem.on('change', function () {
-                   var value = elem.val(),
-                       ext = value.substring(value.lastIndexOf('.') + 1).toLowerCase();   
-
-                   return validFormats.indexOf(ext) !== -1;
-                });
-           };
-        }
+        });
     };
 });
+
+
