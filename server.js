bd00846d21fc331067352be205d8d20daee91e16
shawon046 <shawon56512@gmail.com>
2021-05-06 15:35:44 +0600

    server all ok

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
+app.use('/static', express.static('styling and scripts'))
 
 app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname,'/digital clock.html'))
